c11c7ef9609d9e9a08a4104bf53740f11d1c04a6
Yanxin GONG <yagong@egencia.com>
2017-07-30 23:00:20 +0200

    version 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var casper = require('casper').create({
   verbose: true,
-  logLevel: 'debug',
+  logLevel: 'info',
   pageSettings: {
     userAgent: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
   },
@@ -10,7 +10,8 @@ var casper = require('casper').create({
   }
 })
 
-const stepCount = {}
+var stepCount = {}
+var resultJSON
 
 function clickPassword (password) {
   var self = this
@@ -57,32 +58,79 @@ casper
       this.waitForResource('udc-vue-liste.html', function () {
             // begin parsing json
         this.log('beging parsing')
+        var self = this
+        this.page.switchToChildFrame(0) // this allow to switch to the ifram that contains the account info
+        resultJSON = this.evaluate(function () {
+          function parseMoney (string) {
+                    // string is like
+            var money = {}
+                    // phantomjs has bug for replace /\./, need to use split
+            string = string.replace(/ /g, '').split('.').join('').replace(/,/, '.')
+            money.unit = string[string.length - 1]
+            money.value = parseFloat(string.split(money.unit)[0])
+
+            return money
+          }
+
+          function isHidden (el) {
+            var style = window.getComputedStyle(el)
+            return (style.display === 'none')
+          }
+
+          __utils__.log('------------------------------------------------')
+          try {
+                    // var accountSection = document.querySelectorAll('section.liste-famille-compte > ul.list-vue1');
+                    // __utils__.log(accountSection.innerHTML)
+            var accountFamily = document.querySelectorAll('section.liste-famille-compte > ul.list-vue1 > li')
+            var accountInfoArray = []
+            for (var i = 0; i < accountFamily.length; i++) {
+              var accountElement = accountFamily[i]
+              if (!isHidden(accountElement)) {
+                __utils__.log('++++++++++++++++++++++++++++')
+                var accountName = accountElement.querySelector('div.infos-compte').querySelector('.js-goto-rop').textContent.trim()
+                var accountNumber = accountElement.querySelector('div.infos-compte').textContent.trim().replace(/ /g, '').replace(/\n+/g, '\n').split('\n')[1]
+                            // solde
+                var soldeElement = accountElement.querySelector('.udc-solde')
+                var solde
+                if (soldeElement) {
+                  var soldeString = soldeElement.textContent.trim()
+                  __utils__.log(soldeString)
+                  solde = parseMoney(soldeString)
+                }
+                            // a venir
+                var aVenirElement = accountElement.querySelector('.a-venir')
+                var aVenirSolde
+                if (aVenirElement) {
+                  var aVenirString = aVenirElement.textContent.trim().split(':')[1]
+                  aVenirSolde = parseMoney(aVenirString)
+                  __utils__.log('aVenirSolde ' + JSON.stringify(aVenirSolde, null, 2))
+                }
+                var accountInfo = {
+                  '1_accountName': accountName,
+                  '2_accountNumber': accountNumber,
+                  '3_solde': solde,
+                  '4_aVenir': aVenirSolde
+                }
+                __utils__.log('------')
+                __utils__.log('accountInfo ' +
+                                JSON.stringify(accountInfo, null, 2))
+                accountInfoArray.push(accountInfo)
+              }
+            }
+            return accountInfoArray
+          } catch (err) {
+            __utils__.log('error !\n' + err, 'error')
+          }
+        })
+        this.log('array is \n' + JSON.stringify(resultJSON, null, 2))
       }, function () {
         this.log('no account list', 'error')
         takeScreenshot.call(this, 'error no account list')
       }, 10000)
     })
-    // .then(function () {
-    //     this.waitUntilVisible('section.liste-famille-compte > header.udc-ligne-titre', function () {
-    //         // begin parsing json
-    //         this.log('beging parsing')
-    //     }, function () {
-    //         this.log('no account list', 'error')
-    //         takeScreenshot.call(this, 'error no account list')
-    //     }, 10000)
-    // })
-    // .then(function () {
-    //   this.waitFor(function () {
-    //     return this.evaluate(function () {
-    //       return document.querySelectorAll('section.liste-famille-compte > ul.udc-liquidite').length > 0
-    //     })
-    //   }, function () {
-    //         // begin parsing json
-    //   }, function () {
-    //     this.log('no account list', 'error')
-    //     takeScreenshot.call(this, 'error no account list')
-    //   }, 10000)
-    // })
     .then(takeScreenshot)
 
-casper.run()
+casper.run(function () {
+    console.log('account are: \n' + JSON.stringify(resultJSON, null, 2))
+    this.exit()
+})
